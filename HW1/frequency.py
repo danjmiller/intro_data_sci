5b2d7e1d5efc4527a37b85a9b211aa7e0b4522da
Dan Miller <dmill178@gmail.com>
2014-07-06 21:42:12 -0700

    Q4 passing auto grader

---
+++ HW1/frequency.py
@@ -0,0 +1,50 @@
+import sys
+import json
+
+def load_twitter_data(tweet_file):
+    tweets = []
+
+    for line in tweet_file:
+        tweet = json.loads(line)
+
+        if 'text' in tweet:
+            tweets.append(tweet['text'])
+
+    return tweets
+
+def calculate_term_freq(tweets):
+    counts = {}
+    total_terms = 0;
+
+    for tweet in tweets:
+        words = tweet.split(" ")
+        total_terms += len(words)
+
+        for word in words:
+            word = word.replace("\n"," ")
+            if word.lower() not in counts:
+                counts[word.lower()] = 1
+            else:
+                counts[word.lower()] += 1
+
+    freqs = []
+    for term in counts:
+        freqs.append((term, float(float(counts[term])/float(total_terms))))
+
+    return freqs
+
+def main():
+
+    tweet_file = open(sys.argv[1])
+    tweets = load_twitter_data(tweet_file)
+
+    freqs = calculate_term_freq(tweets)
+
+    for term in freqs:
+        if term[0] is not "":
+            print u"{:s} {:03.4f}".format(term[0].strip(), term[1])
+
+
+
+if __name__ == '__main__':
+    main()
