8ac4c11181312cb62deb26f030d6ecd81c588d89
Daniel Miller <damiller@lab126.com>
2014-07-26 17:43:32 -0700

    Working Solution HW3 Q3

--- HW3/friend_count.py
+++ HW3/friend_count.py
@@ -1,1 +1,29 @@
-__author__ = 'damiller'
+import MapReduce
+import sys
+
+"""
+Word Count Example in the Simple Python MapReduce Framework
+"""
+
+mr = MapReduce.MapReduce()
+
+# =============================
+# Do not modify above this line
+
+def mapper(record):
+    # key: person name
+    # value: friend count
+    person = record[0]
+    mr.emit_intermediate(person, 1)
+
+def reducer(key, list_of_values):
+    # key: person
+    # value: list of friend counts
+
+    mr.emit((key, len(list_of_values)))
+
+# Do not modify below this line
+# =============================
+if __name__ == '__main__':
+  inputdata = open(sys.argv[1])
+  mr.execute(inputdata, mapper, reducer)
